c2695587c12c74fdced7f94c7e29d1282d2630c4
SalterFernandes <salter.sfernandes@gmail.com>
2022-09-24 12:12:02 +0100

    adicionados banners Ajuste de css

--- content/pages/modules/destaques.php
+++ content/pages/modules/destaques.php
@@ -1,5 +1,5 @@
 <div class="col-lg-3">
-            <div class="card">
+            <div class="card dest_card">
                 <img src="https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6Im9naWlpdmx1ZXhvOTItU1REVlRMUFQiLCJ3IjpbeyJmbiI6IjZtZ2p3bHA3a2dkYjItU1REVlRMUFQiLCJzIjoiMTYiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.Q8g43yL9XsvgGrjj8YtH7EkR4J3-iB7HO_qk9iY-IPE/image;s=320x240" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Peugeot 3008 1.5 BlueHDi Style</h5>
@@ -9,8 +9,8 @@
             </div>
         </div>
         <div class="col-lg-3">
-            <div class="card">
-                <img src="https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6Im5wd29udnMyeTI4cjItU1REVlRMUFQiLCJ3IjpbeyJmbiI6IjZtZ2p3bHA3a2dkYjItU1REVlRMUFQiLCJzIjoiMTYiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.8GXL7INTEec5Gkx_BFQa9KMeP35cvh0Na4LOV3nUYHQ/image;s=320x240" class="card-img-top" alt="...">
+            <div class="card ">
+                <img  src="https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6Im5wd29udnMyeTI4cjItU1REVlRMUFQiLCJ3IjpbeyJmbiI6IjZtZ2p3bHA3a2dkYjItU1REVlRMUFQiLCJzIjoiMTYiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.8GXL7INTEec5Gkx_BFQa9KMeP35cvh0Na4LOV3nUYHQ/image;s=320x240" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Mercedes-Benz CLS 450 4Matic AMG Line</h5>
                     <p class="card-text">2019 . 67.453 kms . Gasolina . Maio . 389 cv </p>
@@ -19,7 +19,7 @@
             </div>
         </div>
         <div class="col-lg-3">
-            <div class="card">
+            <div class="card dest_card">
                 <img src="https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6InltZ25ja2JqdXA4MjItU1REVlRMUFQiLCJ3IjpbeyJmbiI6IjZtZ2p3bHA3a2dkYjItU1REVlRMUFQiLCJzIjoiMTYiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.juc8JyU-37L7Fta-OtBaQvssKzDCQU8rPLK5e5JG4x0/image;s=320x240" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Mercedes-Benz S 350 d Longo</h5>
@@ -29,7 +29,7 @@
             </div>
         </div>
         <div class="col-lg-3">
-            <div class="card">
+            <div class="card dest_card">
                 <img src="https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6ImNjdnJkaDY1NzhuYTItU1REVlRMUFQiLCJ3IjpbeyJmbiI6IjZtZ2p3bHA3a2dkYjItU1REVlRMUFQiLCJzIjoiMTYiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.DIbtgUfFjhelTcv_yWewDiHawtKymVFGrhs9r1rQYvo/image;s=320x240" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">BMW X5 25 d sDrive Comfort 7L</h5>
