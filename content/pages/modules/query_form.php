46a0f44e3f89518ab93817ca38a0a12721931d19
Bruno <104459031+baspercode@users.noreply.github.com>
2022-09-29 21:15:48 +0100

    form formatado e responsivo

--- content/pages/modules/query_form.php
+++ content/pages/modules/query_form.php
@@ -1,30 +1,7 @@
 <div class="row">
-                <div class="col-6 mt-2">
-                    <label for="cars">Modelos</label>
-                    <select class="form-select" aria-label="modelos">
-                    <option selected>Modelo</option>
-                    <option value="1">116</option>
-                    <option value="2">Série 2</option>
-                    <option value="3">Série 3</option>
-                    <option value="4">Série 4</option>
-                    <option value="5">Série 4</option>
-                    </select>
-                </div>
-            <div class="col-6 mt-2">
-                <label for="cars">Sub-modelos</label>
-                    <select class="form-select" aria-label="sub-modelos">
-                    <option selected>Sub-modelo</option>
-                    <option value="1">114</option>
-                    <option value="2">116</option>
-                    <option value="3">118</option>
-                    <option value="4">120</option>
-                    <option value="5">123</option>
-                    </select>
-            </div>
-
-            <div class="col-6 mt-2">
-            <label for="cars">Segmento</label>
-            <select class="form-select" aria-label="segmentos">
+    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mt-2">
+        <label for="cars">Segmento</label>
+        <select class="form-select" aria-label="segmentos">
                 <option value="volvo" selected disabled hidden>Segmento</option>
                 <option value="volvo">Cabrio</option>
                 <option value="saab">Carrinha</option>
@@ -33,72 +10,154 @@
                 <option value="volvo">Monovolume</option>
                 <option value="saab">Pequeno citadino</option>
                 <option value="mercedes">Sedan</option>
-            </select>
-            </div>
-            <div class="col-6 mt-2">
-                <label for="cars">Marcas</label>
-                <select class="form-select" aria-label="precos">
-                    <option value="volvo" selected disabled hidden>Marca</option>
-                    <option value="volvo">BMW</option>
-                    <option value="saab">Mercedez-Benz</option>
-                    <option value="mercedes">Renault</option>
-                    <option value="audi">Peugeot</option>
-                    <option value="volvo">VW</option>
-                    <option value="saab">Audi</option>
-                    <option value="mercedes">Citroén</option>
-                    <option value="saab">SEAT</option>
-                    <option value="mercedes">Abarth</option>
-                    <option value="volvo">AC</option>
-                    <option value="saab">Acura</option>
-                    <option value="mercedes">Aiways</option>
-                    <option value="saab">Aixam</option>
-                    <option value="mercedes">Alfa Romeo</option>    
-                </select>
-            </div>
-            <label for="cars">Ano</label>
-            <div class="col-3 mt-2">
-                <select class="form-select" aria-label="anos">
-                    <option value="">De</option>
-                    <option value="">2020</option>
-                    <option value="">2010</option>
-                    <option value="">2000</option>
-                    <option value="">1990</option>
-                    <option value="">1980</option>
-                </select>
-            </div>
-            <div class="col-3 mt-2">
-                <select class="form-select" aria-label="anos">
-                    <option value="">Até</option>
-                    <option value="">2020</option>
-                    <option value="">2010</option>
-                    <option value="">2000</option>
-                    <option value="">1990</option>
-                    <option value="">1980</option>
-                </select>
-            </div>
-            <div class="col-6 mt-2">
-                <label for="cars">Combustiveis</label>
-                <select class="form-select" aria-label="combustiveis">
-                <option value="">Combustivel</option>
-                <option value="">Diesel</option>
-                <option value="">Elétrico</option>
-                <option value="">Gasolina</option>
-                <option value="">GPL</option>
-                <option value="">Hidrogénio</option>
-                </select>
-            </div>
-            <div class="col-6 mt-2">
-                <label for="cars">Kms</label>
-                <select class="form-select" aria-label="kms">
-                    <option value="50 000km">50000</option>
-                    <option value="100 000km">100000</option>
-                    <option value="150 000km">150000</option>
-                    <option value="200 000km">200000</option>
-                    <option value="250 000km">250000</option>
-                    <option value="300 000km">300000</option>
-                    <option value="350 000km">350000</option>
-                    <option value="400 000km">400000</option>
-                </select>
-            </div>
-            <a class="btn btn-primary mt-5" href="ROTA" role="button">Pesquisa</a> 
+        </select>
+    </div>
+    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mt-2">
+        <label for="cars">Marcas</label>
+        <select class="form-select" aria-label="precos">
+            <option value="volvo" selected disabled hidden>Marca</option>
+            <option value="volvo">BMW</option>
+            <option value="saab">Mercedez-Benz</option>
+            <option value="mercedes">Renault</option>
+            <option value="audi">Peugeot</option>
+            <option value="volvo">VW</option>
+            <option value="saab">Audi</option>
+            <option value="mercedes">Citroén</option>
+            <option value="saab">SEAT</option>
+            <option value="mercedes">Abarth</option>
+            <option value="volvo">AC</option>
+            <option value="saab">Acura</option>
+            <option value="mercedes">Aiways</option>
+            <option value="saab">Aixam</option>
+            <option value="mercedes">Alfa Romeo</option>    
+        </select>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mt-2">
+        <label for="cars">Modelos</label>
+        <select class="form-select" aria-label="modelos">
+            <option selected>Modelo</option>
+            <option value="1">116</option>
+            <option value="2">Série 2</option>
+            <option value="3">Série 3</option>
+            <option value="4">Série 4</option>
+            <option value="5">Série 4</option>
+        </select>
+    </div>
+    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mt-2">
+        <label for="cars">Sub-modelos</label>
+        <select class="form-select" aria-label="sub-modelos">
+            <option selected>Sub-modelo</option>
+            <option value="1">114</option>
+            <option value="2">116</option>
+            <option value="3">118</option>
+            <option value="4">120</option>
+            <option value="5">123</option>
+        </select>
+    </div>
+
+    <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 mt-2">
+    <label for="precos">Preço</label>
+        <select id="precos"class="form-select" aria-label="precos" name="preco1">
+            <option selected>de</option>
+            <option value="250">250 EUR</option>
+            <option value="500">500 EUR</option>
+            <option value="1000">1 000 EUR</option>
+            <option value="1500">1 500 EUR</option>
+            <option value="2000">2 000 EUR</option>
+            <option value="2500">2 500 EUR</option>
+            <option value="3000">3 000 EUR</option>
+            <option value="3500">3 500 EUR</option>
+            <option value="4000">4 000 EUR</option>
+            <option value="4500">4 500 EUR</option>
+        </select>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 mt-2">
+        <label for="precos"></label>
+        <select id="precos"class="form-select" aria-label="precos" name="preco2">
+            <option selected>até</option>
+            <option value="250">250 EUR</option>
+            <option value="500">500 EUR</option>
+            <option value="1000">1 000 EUR</option>
+            <option value="1500">1 500 EUR</option>
+            <option value="2000">2 000 EUR</option>
+            <option value="2500">2 500 EUR</option>
+            <option value="3000">3 000 EUR</option>
+            <option value="3500">3 500 EUR</option>
+            <option value="4000">4 000 EUR</option>
+            <option value="4500">4 500 EUR</option>
+        </select>
+    </div>
+
+    <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 mt-2">
+    <label for="cars">Ano</label>
+        <select class="form-select" aria-label="anos">
+            <option value="">De</option>
+            <option value="">2020</option>
+            <option value="">2010</option>
+            <option value="">2000</option>
+            <option value="">1990</option>
+            <option value="">1980</option>
+        </select>
+    </div>
+    <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 mt-2">
+    <label for="cars"></label>
+        <select class="form-select" aria-label="anos">
+            <option value="">Até</option>
+            <option value="">2020</option>
+            <option value="">2010</option>
+            <option value="">2000</option>
+            <option value="">1990</option>
+            <option value="">1980</option>
+        </select>
+    </div>
+
+    <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 mt-2">
+        <label for="cars">Combustiveis</label>
+        <select class="form-select" aria-label="combustiveis">
+            <option value="">Combustivel</option>
+            <option value="">Diesel</option>
+            <option value="">Elétrico</option>
+            <option value="">Gasolina</option>
+            <option value="">GPL</option>
+            <option value="">Hidrogénio</option>
+        </select>
+    </div>
+
+    <!-- Parte que o rafael tem de apagar -->
+    <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 mt-2">
+        <label for="cars" class="bold">Quilómetros</label>
+        <select class="form-select" aria-label="kms">
+            <option selected>de</option>
+            <option value="50 000km">5 0000 km</option>
+            <option value="100 000km">1 00000 km</option>
+            <option value="150 000km">1 50000 km</option>
+            <option value="200 000km">2 00000 km</option>
+            <option value="250 000km">25 0000 km</option>
+            <option value="300 000km">30 0000 km</option>
+            <option value="350 000km">35 0000 km</option>
+            <option value="400 000km">40 0000 km</option>
+        </select>
+    </div>
+    <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 mt-2">
+        <label for="precos" ></label>
+        <select id="precos"class="form-select" aria-label="precos" name="preco2">
+            <option selected>até</option>
+            <option value="50 000km">5 0000 km</option>
+            <option value="100 000km">1 00000 km</option>
+            <option value="150 000km">1 50000 km</option>
+            <option value="200 000km">2 00000 km</option>
+            <option value="250 000km">25 0000 km</option>
+            <option value="300 000km">30 0000 km</option>
+            <option value="350 000km">35 0000 km</option>
+            <option value="400 000km">40 0000 km</option>
+        </select>
+    </div>
+
+    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 mt-2">
+        <a class="btn btn-outline-secondary text-dark fw-bold mt-3 form-control border-0" href="ROTA" role="button"><i class="bi bi-sliders"></i>&nbsp Pesquisa Avançada</a> 
+    </div>
+    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 mt-2 mb-3">
+        <a class="btn btn-danger mt-3 form-control" href="ROTA" role="button">VER 30 000 ANUNCIOS</a> 
+    </div>
 </div>
