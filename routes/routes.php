3d13ca123a9b540d83baca8aeddea26b3e2dbbb1
SalterFernandes <salter.sfernandes@gmail.com>
2022-09-29 21:16:13 +0100

    Adicionado titulo da pagina de destaques e filtros

--- routes/routes.php
+++ routes/routes.php
@@ -5,8 +5,8 @@ if(isset($_GET['p'])){
         include('content/pages/inicio.php');
     else if($p == "destaques")
         include('content/pages/destaques.php');
-    else if($p == "resultados")
-        include('content/pages/resultados.php');
+    else if($p == "pesquisa")
+        include('content/pages/pesquisa.php');
     else
         include('content/pages/404.php');
 }else if(isset($_GET['pesquisa'])){
