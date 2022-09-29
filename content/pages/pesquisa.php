cf7171ea6dd94aaa7157a96857c59dd22c389ffa
Bruno <104459031+baspercode@users.noreply.github.com>
2022-09-29 22:41:46 +0100

    Update pesquisa.php
    filtro na pesquisa

--- content/pages/pesquisa.php
+++ content/pages/pesquisa.php
@@ -1,6 +1,16 @@
 <div class="container mt-5">
     <div class="row">
-
+        <div class="col-2 mt-2">
+            <select class="form-select" aria-label="sub-modelos">
+                <option selected>Filtrar...</option>
+                <option value="1">Preço Ascendente</option>
+                <option value="2">Preço Descendente</option>
+                <option value="3">Nome Ascendente</option>
+                <option value="4">Nome Descendente</option>
+                <option value="4">Kms Ascendente</option>
+                <option value="4">Nome Descendente</option>
+            </select>
+        </div>
     </div>
 </div>
 
