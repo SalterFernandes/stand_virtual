129db2f1c740f5b697a65907907ebd7958b9b43b
MaradoMoreira <thranmoreira@gmail.com>
2022-09-29 22:27:16 +0100

    Cards Destaques Pedro Moreira

--- content/footer.php
+++ content/footer.php
@@ -1,8 +1,11 @@
 </main>
 <?php date_default_timezone_set('Europe/Lisbon'); ?>
+
 <footer class="text-center mt-5">
     <p>Copyright <?= date('Y');?> Stand Virtual</p>
 </footer>
+
+
 <!-- BOOTSTRAP JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <!-- MEU JS -->
