c85449239ae87b5da831c7daf7152a9ed2472c77
SalterFernandes <salter.sfernandes@gmail.com>
2022-09-29 22:41:03 +0100

    Fix layout destaques.php Adicionado link no nav para destaques

--- content/pages/destaques.php
+++ content/pages/destaques.php
@@ -4,17 +4,20 @@
      <div class="col-12 text-center">
             <h1> Destaques </h1>
      </div>
-     <div class="col-2 mt-2">
-            <select class="form-select" aria-label="sub-modelos">
-                <option selected>Filtrar...</option>
-                <option value="1">Preço Ascendente</option>
-                <option value="2">Preço Descendente</option>
-                <option value="3">Nome Ascendente</option>
-                <option value="4">Nome Descendente</option>
-                <option value="4">Kms Ascendente</option>
-                <option value="4">Kms Descendente</option>
-            </select>
+        <div class="row mb-3">
+            <div class="col-2 mt-2">
+                <select class="form-select" aria-label="sub-modelos">
+                    <option selected>Filtrar...</option>
+                    <option value="1">Preço Ascendente</option>
+                    <option value="2">Preço Descendente</option>
+                    <option value="3">Nome Ascendente</option>
+                    <option value="4">Nome Descendente</option>
+                    <option value="4">Kms Ascendente</option>
+                    <option value="4">Kms Descendente</option>
+                </select>
+            </div>
         </div>
+
         <div class="col-lg-3">
             <div class="car dest_card">
                 <img src="https://ireland.apollo.olxcdn.com/v1/files/eyJmbiI6ImNjdnJkaDY1NzhuYTItU1REVlRMUFQiLCJ3IjpbeyJmbiI6IjZtZ2p3bHA3a2dkYjItU1REVlRMUFQiLCJzIjoiMTYiLCJwIjoiMTAsLTEwIiwiYSI6IjAifV19.DIbtgUfFjhelTcv_yWewDiHawtKymVFGrhs9r1rQYvo/image;s=320x240" class="card-img-top" alt="...">
